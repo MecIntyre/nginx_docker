d399fdc0fe1512e1332b67960bb29c5a607fd0b9
Stephan Thiele <abuesswalker@gmail.com>
2021-08-31 12:51:18 +0200

    #!/bin/sh hinzugefügt

--- pre.sh
+++ pre.sh
@@ -1,4 +1,5 @@
-# Delete the runner and virtual box directories for cleaning up and prevent failures
+#!/bin/sh
 
+# Delete the runner and virtual box directories for cleaning up and prevent failures
 [ -d "/home/gitlab-runner/builds/" ] && rm -rf /home/gitlab-runner/builds
 [ -d "/home/VirtualBox VMs/nginx/" ] && rm -rf /home/VirtualBox\ VMs/nginx
