c8f3585de8eec673a74f550e8e9502bfd5a5aaa1
Stephan Thiele <abuesswalker@gmail.com>
2021-08-31 14:54:12 +0200

    Testing 6

--- pre.sh
+++ pre.sh
@@ -1,3 +1,3 @@
 # Delete the runner and virtual box directories for cleaning up and prevent failures
 [ -d "/home/gitlab-runner/builds/" ] && rm -rf /home/gitlab-runner/builds/qEsZNt7P
-[ -d "/home/VirtualBox VMs/nginx/" ] && rm -rf /home/VirtualBox\ VMs/nginx
+[ -d "/home/VirtualBox VMs/nginx/" ] && rm -rf "/home/VirtualBox VMs/nginx"
