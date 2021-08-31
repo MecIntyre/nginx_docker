93690d30df3d4be1572e7290e40f35aab7b48955
Stephan Thiele <abuesswalker@gmail.com>
2021-08-31 12:41:13 +0200

    kommentar- und strukturanpassung

--- pre.sh
+++ pre.sh
@@ -1,2 +1,4 @@
+# Delete the runner and virtual box directories for cleaning up and prevent failures
+
 [ -d "/home/gitlab-runner/builds/" ] && rm -rf /home/gitlab-runner/builds
 [ -d "/home/VirtualBox VMs/nginx/" ] && rm -rf /home/VirtualBox\ VMs/nginx
